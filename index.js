fd0281a60d5c8fd179a1698a20571270d2829e1c
tscholl44 <tscholl4444@gmail.com>
2024-10-18 16:05:28 -0400

    installed and required express along with morgan to make three different GET requests

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'), // import built in node modules fs and path 
+  path = require('path');
+
+const app = express();
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Only the best movies!');
+});
+
+app.use('/documentation.html', express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
