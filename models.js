cca372a1ad6ad973db4d604e7318e913b9292198
tscholl44 <tscholl4444@gmail.com>
2024-11-29 20:05:26 -0500

    All lowercase keys

--- models.js
+++ models.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 
 let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {
-      Name: String,
-      Description: String
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+      name: String,
+      description: String
     },
-    Director: {
-      Name: String,
-      Bio: String
+    director: {
+      name: String,
+      bio: String
     },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+    actors: [String],
+    imagePath: String,
+    featured: Boolean
   });
   
   let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthday: Date,
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
   
   let Movie = mongoose.model('Movie', movieSchema);
