579d41bcbc99fa0e775e8f1916c36e62ca76a148
tscholl44 <tscholl4444@gmail.com>
2025-02-07 15:57:01 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -23,6 +23,10 @@ passport.use(
             message: 'Incorrect name or password.',
           });
         }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
         console.log('finished');
         return callback(null, user);
       })
